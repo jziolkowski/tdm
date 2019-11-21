b09e341587c6e7d7642b2de250cd51943032d66d
jziolkowski <jacek@ziolkowscy.com>
2019-11-21 02:21:59 +0100

    added select all/none to clear LWT dialog

--- GUI/ClearLWT.py
+++ GUI/ClearLWT.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QDialog, QPushButton, QDialogButtonBox, QListWidget, QListWidgetItem, QLabel
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QListWidget, QListWidgetItem, QLabel
 
-from GUI import VLayout, GroupBoxV, HLayout, GroupBoxH
+from GUI import VLayout
 
 
 class ClearLWTDialog(QDialog):
@@ -14,18 +14,29 @@ class ClearLWTDialog(QDialog):
 
         vl = VLayout()
 
+        sel_btns = QDialogButtonBox()
+        sel_btns.setCenterButtons(True)
+        btnSelAll = sel_btns.addButton("Select all", QDialogButtonBox.ActionRole)
+        btnSelNone = sel_btns.addButton("Select none", QDialogButtonBox.ActionRole)
+
         self.lw = QListWidget()
 
         for lwt in self.env.lwts:
             itm = QListWidgetItem(lwt)
-            itm.setCheckState(Qt.Checked)
+            itm.setCheckState(Qt.Unchecked)
             self.lw.addItem(itm)
+
+        btnSelAll.clicked.connect(lambda: self.select(Qt.Checked))
+        btnSelNone.clicked.connect(lambda: self.select(Qt.Unchecked))
 
         btns = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Close)
         btns.accepted.connect(self.accept)
         btns.rejected.connect(self.reject)
 
-        vl.addWidgets([QLabel("Select LWTs to be cleared:"), self.lw, btns])
+        vl.addWidgets([sel_btns, self.lw, btns])
         self.setLayout(vl)
 
-
+    def select(self, state):
+        for row in range(self.lw.count()):
+            itm = self.lw.item(row)
+            itm.setCheckState(state)
