f627976fcf733d4da85a9a2aee223c05e71d382f
Barbudor <barbudor@barbudor.net>
2021-06-20 15:23:00 +0200

    support TIMERS for Tasmota >= 9.4.0.5

--- GUI/Timers.py
+++ GUI/Timers.py
@@ -226,7 +226,11 @@ class TimersDialog(QDialog):
             if self.device.reply == "RESULT" or self.device.reply == "TIMERS":
                 try:
                     payload = loads(msg)
-                    first = list(payload)[0]
+                    all = list(payload)
+                    first = all[0]
+                    logging.debug("TIMERS payload: %s", payload)
+                    logging.debug("TIMERS list: %s", list(payload))
+                    logging.debug("TIMERS first  : %s", first)
 
                 except JSONDecodeError as e:
                     error = "Timer loading error", "Can't load the timer from device.\n{}".format(e)
@@ -237,9 +241,19 @@ class TimersDialog(QDialog):
                     if first == 'Timers':
                         self.gbTimers.setChecked(payload[first] == "ON")
 
+                        # Tasmota >= 9.4.0.5 : There is only 1 message that covers all
+                        #   { "Timers": "ON", "Timer1":{"Enable":0,"Mode":0,"Time":"00:00","Window":0,"Days":"0000000","Repeat":0,"Action":0}, ....
+                        if len(all) > 1: # If first is "Timers" but they are more, then it is Tasmota >= 9.4.0.5
+                            payload.pop("Timers")
+                            self.timers.update(payload)
+                            self.loadTimer(self.cbTimer.currentText())
+
+                    # Tasmota < 9.4.0.5 : There are a total of 4 messages in reply to `Timers` command:
+                    #   {"Timers": "ON" }
+                    #   {"Timers1" : {"Timer1":{"Enable":0,"Mode":0,"Time":"00:00","Window":0,"Days":"0000000","Repeat":0,"Action":0}, ....
+                    #   ...
+                    #   {"Timers4" : {"Timer13":{"Enable":0,"Mode":0,"Time":"00:00","Window":0,"Days":"0000000","Repeat":0,"Action":0}, ....
                     elif first.startswith('Timers'):
                         self.timers.update(payload[first])
-
-                    if first == 'Timers4':
-                        self.loadTimer(self.cbTimer.currentText())
-
+                        if first == 'Timers4':
+                            self.loadTimer(self.cbTimer.currentText())
